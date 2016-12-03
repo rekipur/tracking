e0a700b4b26084ad64de0e9157c6095f386c2468
Pranzela Reki Purnama <pranzela.reki88@gmail.com>
2016-12-03 15:21:45 +0700

    Update Login role dan Registraion

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/about', 'MyController@showAbout');
+
+Route::group(['prefix'=>'admin', 'middleware'=>['auth']], function () {
+	// Route diisi disini
+	Route::resource('authors', 'AdminController');
+});
